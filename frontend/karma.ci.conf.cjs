7596eb9dda96b4aefbb0ea2fa9fe2b8ddbb1ecf4
Oscar <oskar_prp94@hotmail.com>
2026-04-09 13:43:27 +0200

    Fix CI frontend tests with Chrome no-sandbox launcher

---
+++ frontend/karma.ci.conf.cjs
@@ -0,0 +1,11 @@
+module.exports = function (config) {
+  config.set({
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
+      },
+    },
+    browsers: ['ChromeHeadlessNoSandbox'],
+  });
+};
