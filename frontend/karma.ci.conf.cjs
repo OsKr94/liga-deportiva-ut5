88a22e88bc813b7d6e986315fea1c3728730a582
Oscar <oskar_prp94@hotmail.com>
2026-04-09 13:49:28 +0200

    Fix Karma CI config for frontend tests

--- frontend/karma.ci.conf.cjs
+++ frontend/karma.ci.conf.cjs
@@ -1,5 +1,32 @@
+const path = require('path');
+
 module.exports = function (config) {
   config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular/build'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular/build/plugins/karma'),
+    ],
+    client: {
+      clearContext: false,
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true,
+    },
+    coverageReporter: {
+      dir: path.join(__dirname, './coverage/liga-deportiva'),
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'text-summary' }],
+    },
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
@@ -7,5 +34,7 @@ module.exports = function (config) {
       },
     },
     browsers: ['ChromeHeadlessNoSandbox'],
+    singleRun: true,
+    restartOnFileChange: false,
   });
 };
